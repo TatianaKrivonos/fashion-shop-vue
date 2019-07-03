563d912c1255ef49ad0c65a404c7a97cb02a8c31
TatianaKrivonos <konan_air.96@mail.ru>
2019-07-03 22:47:44 +0300

    созданы основные компоненты

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+<script>
+  import PageHeader from '@/components/PageHeader.vue'
+  import Banner from '@/components/Banner.vue'
+  import EcommerceSigns from '@/components/EcommerceSigns.vue'
+  import Featured from '@/components/Featured.vue'
+  import MainSlider from '@/components/MainSlider.vue'
+  import NewsLetter from '@/components/NewsLetter.vue'
+  import PhotoGallery from '@/components/PhotoGallery.vue'
+  import Sale from '@/components/Sale.vue'
+  import PageFooter from '@/components/PageFooter.vue'
+
+  export default {
+    name: 'home',
+    components: {
+      PageHeader,
+      Banner,
+      EcommerceSigns,
+      Featured,
+      MainSlider,
+      NewsLetter,
+      PhotoGallery,
+      Sale,
+      PageFooter
+    }
+  }
+</script>
