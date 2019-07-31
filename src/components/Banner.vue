220f113b97ef4770267d3f5c526d8b1cd5dc79eb
TatianaKrivonos <konan_air.96@mail.ru>
2019-07-31 23:11:16 +0300

    стилизован banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,8 +1,8 @@
 <template>
 <section class="banner">
   <div class="banner__container">
-    <p class="banner__text">this id fashionshop</p>
-    <p class="banner__text">your fashin and style destination</p>
+    <p class="banner__text">this is fashionshop</p>
+    <p class="banner__text">your fashion and style destination</p>
     <router-link to="/shop" class="banner__btn">shop for women</router-link>
   </div>
 </section>
