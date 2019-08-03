f23d8f7972df4b80f91c3c817acfd9517ffb3457
TatianaKrivonos <konan_air.96@mail.ru>
2019-08-04 00:48:58 +0300

    стилизованы карточки со спец.предложением

--- src/components/InfoCards.vue
+++ src/components/InfoCards.vue
@@ -1,16 +1,20 @@
 <template>
 <div class="info-cards">
   <div class="info-cards__container">
-    <div class="info-cards__card">
-      <p>sale up to</p>
-      <p>kemeks</p>
-      <p>be cool</p>
-      <a href="#">detail more</a>
+    <div class="info-cards__col">
+      <div class="info-cards__card info-cards__card--first">
+        <p>sale up to</p>
+        <p>Kemeks</p>
+        <p>be cool</p>
+        <a href="#">detail more</a>
+      </div>
     </div>
-    <div class="info-cards__card">
-      <p>40%</p>
-      <p>off everything</p>
-      <a href="#">shop now</a>
+    <div class="info-cards__col">
+      <div class="info-cards__card info-cards__card--last">
+        <p>40%</p>
+        <p>off everything</p>
+        <a href="#">shop now</a>
+      </div>
     </div>
   </div>
 </div>
