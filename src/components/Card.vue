55dd399762bc4d7617b3a13cf8a4549f69ddb45c
TatianaKrivonos <konan_air.96@mail.ru>
2019-08-04 22:20:51 +0300

    стилизована карточка товара

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,16 @@
 <template>
 <div class="sale__card-col">
   <article class="product-card">
-    <img src="" alt="product-card" class="product-card__img">
+    <div class="product-card__img">
+       <img src="../assets/img/sale/sale1.jpg" alt="product-card">
+    </div>
     <v-rating
       v-model="rating"
       color="yellow"
     >
     </v-rating>
     <p class="product-card__name">name</p>
-    <p class="product-card__price">price</p>
+    <p class="product-card__price">$price</p>
     <button class="product-card__btn">add to cart</button>
   </article>
 </div>
@@ -18,6 +20,28 @@
     data () {
       return {
         rating: 5
+        // cards: [
+        //   {
+        //    img: '../img/sale/sale1.jpg',
+        //    name: 'Embroidered Linen Shirt',
+        //    price: 30.50
+        //   },
+        //   {
+        //    img: '../img/sale/sale2.jpg',
+        //    name: 'Embroidered Linen Shirt',
+        //    price: 30.50
+        //   },
+        //   {
+        //    img: '../img/sale/sale3.jpg',
+        //    name: 'Embroidered Linen Shirt',
+        //    price: 30.50
+        //   },
+        //   {
+        //    img: '../img/sale/sale4.jpg',
+        //    name: 'Embroidered Linen Shirt'
+        //    price: 30.50
+        //   }
+        // ]
       }
     }
   }
