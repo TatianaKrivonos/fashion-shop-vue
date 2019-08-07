3919a57d77c76a7c5fcd15b30e2fc713ec2b6888
TatianaKrivonos <konan_air.96@mail.ru>
2019-08-07 23:14:26 +0300

    стилизация блока deal

--- src/components/Deal.vue
+++ src/components/Deal.vue
@@ -3,7 +3,7 @@
   <div class="deal__container">
     <div class="deal__descr">
       <p class="deal__label">sale</p>
-      <p>deal o the day 30% on dresses</p>
+      <p>Deal o the Day 30% on Dresses</p>
       <p>pretty yhing floral skater dress</p>
       <p>$28.00</p>
       <div class="deal__timer">
@@ -12,7 +12,7 @@
       <button class="deal__btn">add to cart</button>
     </div>
     <div class="deal__img-wrap">
-      <img src="/" alt="photo" class="deal__img">
+      <img src="../assets/img/deal.jpg" alt="photo" class="deal__img">
     </div>
   </div>
 </div>
