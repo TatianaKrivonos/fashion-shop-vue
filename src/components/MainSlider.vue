195bccf067099be21fe0decf25815eae31ef5be0
TatianaKrivonos <konan_air.96@mail.ru>
2019-08-16 21:20:50 +0300

    установлен hooper

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -13,13 +13,13 @@
       return {
         items: [
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+            src: ''
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+            src: ''
           },
           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+            src: ''
           }
         ]
       }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import Vuelidate from 'vuelidate'
-import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import App from './App.vue'
 import router from './router'
 import './assets/scss/style.scss'
 
-
-Vue.use(Vuetify, {
-   iconfont: 'md'
-})
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
