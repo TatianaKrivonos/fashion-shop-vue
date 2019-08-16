b1b489353da73c4f61900de471f3871958b95c81
TatianaKrivonos <konan_air.96@mail.ru>
2019-08-16 22:44:50 +0300

    стилизован слайдер

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -1,29 +1,84 @@
 <template>
-  <v-carousel hide-controls>
-    <v-carousel-item
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
+  <hooper>
+    <slide>
+      <div class="slider-item">
+        <p>Sale</p>
+        <p>Up to 50% Off</p>
+      </div>
+    </slide>
+    <slide>
+      <div class="slider-item">
+        <p>Sale</p>
+        <p>Up to 50% Off</p>
+      </div>
+    </slide>
+    <slide>
+      <div class="slider-item">
+        <p>Sale</p>
+        <p>Up to 50% Off</p>
+      </div>
+    </slide>
+    <hooper-pagination slot="hooper-addons"></hooper-pagination>
+  </hooper>
 </template>
+
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          {
-            src: ''
-          },
-          {
-            src: ''
-          },
-          {
-            src: ''
-          }
-        ]
-      }
-    }
+import 'hooper/dist/hooper.css';
+import {
+  Hooper,
+  Slide,
+  Pagination as HooperPagination
+  } from 'hooper';
+
+export default {
+  components: {
+    Hooper,
+    Slide,
+    HooperPagination
   }
+}
 </script>
+<style>
+  .slider-item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 10%;
+    height: inherit;
+    background-image: url(../assets/img/slider.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .slider-item p:first-child {
+    font-size: 203px;
+    font-weight: 800;
+  }
+
+  .slider-item p:last-child {
+    font-size: 62px;
+    font-weight: 600;
+  }
+
+  .hooper-indicator {
+    margin-right: 10px;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: 1px solid #fff;
+    background-color: transparent;
+  }
+
+  .hooper-indicator:hover {
+    background-color: #fff;
+  }
+
+  .hooper-indicator.is-active {
+    background-color: #fff;
+  }
+
+  .hooper {
+    height: calc(100vh - 84px);
+  }
+</style>
 
